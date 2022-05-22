cc33e7c51db911c478c8ecc2575e3c4b75dda0c2
Aleksei Surmin <aleks.fandoren@gmail.com>
2022-05-22 20:10:08 +0300

    add logic and endpoints for getting pages and image get/save in Tag service

--- recipe-service/src/main/java/com/surmin/recipe/controller/TagController.java
+++ recipe-service/src/main/java/com/surmin/recipe/controller/TagController.java
@@ -1,9 +1,14 @@
 package com.surmin.recipe.controller;
 
+import com.surmin.recipe.model.Tag;
 import com.surmin.recipe.model.TagDto;
 import com.surmin.recipe.service.TagService;
+import java.io.IOException;
+import java.net.URI;
 import java.util.Collection;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +16,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/v1/tags")
@@ -34,10 +42,35 @@ public class TagController {
         return tagService.getAll();
     }
 
+    @GetMapping("page/{pageId}")
+    public Page<TagDto> getPage(@PathVariable("pageId") Integer pageId) {
+        return tagService.getPage(pageId);
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public TagDto save(@RequestBody TagDto tagDto) {
         return tagService.save(tagDto);
+    }
+
+    @GetMapping("/{entityId}/image")
+    public ResponseEntity<byte[]> getImage(
+            @PathVariable("entityId") String id
+    ) {
+        Tag tag = tagService.getTag(id);
+        byte[] imageAsByteArray = tag.getImageAsByteArray();
+        return ResponseEntity.status(HttpStatus.OK).contentLength(imageAsByteArray.length).body(imageAsByteArray);
+    }
+
+    @PostMapping("/{entityId}/image")
+    public ResponseEntity<Void> uploadImage(
+            @PathVariable("entityId") String id,
+            @RequestParam(value = "image") MultipartFile image
+    ) throws IOException {
+        byte[] imageAsByteArray = image.getBytes();
+        tagService.saveImage(id, imageAsByteArray);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
+        return ResponseEntity.status(HttpStatus.CREATED).location(location).build();
     }
 
     @PutMapping
--- recipe-service/src/main/java/com/surmin/recipe/service/TagService.java
+++ recipe-service/src/main/java/com/surmin/recipe/service/TagService.java
@@ -4,6 +4,11 @@ import com.surmin.recipe.mapper.TagMapper;
 import com.surmin.recipe.model.Tag;
 import com.surmin.recipe.model.TagDto;
 import com.surmin.recipe.repository.TagRepository;
+import java.io.IOException;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,5 +16,36 @@ public class TagService extends CrudService<TagDto, Tag, TagRepository>{
 
     public TagService(TagRepository tagRepository) {
         super(TagMapper.INSTANCE, tagRepository);
+    }
+
+    public static final int IMAGE_BYTES_LIMIT = 1 << 20;
+
+    public Page<TagDto> getPage(Integer pageId) {
+        Pageable pageable = PageRequest.of(pageId, 8);
+        return getAll(pageable);
+    }
+
+    public Tag getTag(String entityId) {
+        Optional<Tag> tagOptional = getMongoRepository().findById(entityId);
+        if (tagOptional.isEmpty()) {
+            throw new IllegalArgumentException("TAG_WAS_NOT_FOUND");
+        }
+        return tagOptional.get();
+    }
+
+    public void saveImage(String entityId, byte[] imageAsByteArray) throws IOException {
+        if (imageAsByteArray == null || imageAsByteArray.length <= 0) {
+            throw new IllegalArgumentException("IMAGE_IS_EMPTY");
+        }
+        if (imageAsByteArray.length > IMAGE_BYTES_LIMIT) {
+            throw new IllegalArgumentException("IMAGE_IS_OVERSIZED");
+        }
+        Optional<Tag> tagOptional = getMongoRepository().findById(entityId);
+        if (tagOptional.isEmpty()) {
+            throw new IllegalArgumentException("TAG_WAS_NOT_FOUND");
+        }
+        Tag tag = tagOptional.get();
+        tag.setImageAsByteArray(imageAsByteArray);
+        getMongoRepository().save(tag);
     }
 }
