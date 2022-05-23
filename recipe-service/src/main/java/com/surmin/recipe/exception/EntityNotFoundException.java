ca66f3754b7d7dcd87e70403e7674767ddf2f727
Aleksei Surmin <aleks.fandoren@gmail.com>
2022-05-23 16:26:29 +0300

    add EntityNotFoundException

---
+++ recipe-service/src/main/java/com/surmin/recipe/exception/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.surmin.recipe.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException (String message) {
+        super(message);
+    }
+}
