5148632905fc22ca678ea4325658595c02a14b4d
Aleksei Surmin <aleks.fandoren@gmail.com>
2022-05-24 21:00:37 +0300

    add tests for tag service and controller. Add corresponding dependencies

---
+++ recipe-service/src/test/java/com/surmin/recipe/controller/TagControllerTest.java
@@ -0,0 +1,59 @@
+package com.surmin.recipe.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import com.surmin.recipe.model.TagDto;
+import com.surmin.recipe.service.TagService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+@TestPropertySource(properties = {"spring.cloud.config.discovery.enabled=false", "spring.cloud.config.enabled=false",
+        "eureka.client.enabled=false"})
+@WebMvcTest(TagController.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TagControllerTest {
+
+    private static final String TAG_PATH = "/api/v1/tags";
+    private static final Logger LOGGER = LogManager.getLogger(TagControllerTest.class);
+
+    @MockBean
+    private TagService tagService;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeAll
+    public void setUp() {
+        TagDto tagDto = new TagDto();
+        tagDto.setName("Name");
+        tagDto.setDescription("Description");
+        tagService.save(tagDto);
+    }
+
+    @Test
+    public void getTagsShouldReturnList() throws Exception {
+        LOGGER.info("GET URL {}", TAG_PATH);
+
+        mockMvc.perform(get(TAG_PATH))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$").isArray());
+
+    }
+
+    @AfterAll
+    public void drop() {
+        tagService.getAll().forEach((dto) ->tagService.delete(dto));
+    }
+}
---
+++ recipe-service/src/test/java/com/surmin/recipe/service/TagServiceTest.java
@@ -0,0 +1,70 @@
+package com.surmin.recipe.service;
+
+import com.surmin.recipe.exception.EntityNotFoundException;
+import com.surmin.recipe.model.TagDto;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(properties = {"spring.cloud.config.discovery.enabled=false", "spring.cloud.config.enabled=false",
+        "eureka.client.enabled=false"})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TagServiceTest {
+
+    @Autowired
+    private TagService tagService;
+
+    @BeforeAll
+    public void setUp() {
+        TagDto tagDto = new TagDto();
+        tagDto.setName("Fruit");
+        tagDto.setDescription("Some description of fruit tag");
+        tagService.save(tagDto);
+
+        tagDto = new TagDto();
+        tagDto.setName("Meat");
+        tagDto.setDescription("Some description of meat tag");
+        tagService.save(tagDto);
+    }
+
+    @AfterAll
+    public void tearDown() {
+        tagService.getAll().forEach((dto) ->tagService.delete(dto));
+    }
+
+    @Test
+    public void countTags() {
+        Assertions.assertEquals(2, tagService.getAll().size());
+    }
+
+    @Test
+    public void saveThrowsIllegalArgumentExceptionIfEntityIdSpecified() {
+        TagDto tagDto = new TagDto();
+        tagDto.setEntityId("SomeEntityId");
+        tagDto.setName("SomeTag");
+        tagDto.setDescription("Some long description");
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> tagService.save(tagDto));
+    }
+
+    @Test
+    public void saveThrowsEntityNotFoundExceptionIfTagDtoIsNull() {
+        TagDto tagDto = null;
+
+        Assertions.assertThrows(EntityNotFoundException.class, () -> tagService.save(tagDto));
+    }
+
+    @Test
+    public void getPageCount() {
+        Page<TagDto> page = tagService.getPage(0);
+
+        Assertions.assertEquals(1, page.getTotalPages());
+    }
+}
