1407640b54cb8aad2676c2b3de10fb5eaedb08ea
Aleksei Surmin <aleks.fandoren@gmail.com>
2022-05-30 21:02:36 +0300

    add methods to product service and corresponding endpoints

--- recipe-service/src/main/java/com/surmin/recipe/controller/ProductController.java
+++ recipe-service/src/main/java/com/surmin/recipe/controller/ProductController.java
@@ -1,9 +1,14 @@
 package com.surmin.recipe.controller;
 
+import com.surmin.recipe.model.Product;
 import com.surmin.recipe.model.ProductDto;
 import com.surmin.recipe.service.ProductService;
+import java.io.IOException;
+import java.net.URI;
 import java.util.Collection;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +16,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -34,10 +42,35 @@ public class ProductController {
         return productService.getAll();
     }
 
+    @GetMapping("page/{pageId}")
+    public Page<ProductDto> getPage(@PathVariable("pageId") Integer pageId) {
+        return productService.getPage(pageId);
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ProductDto save(@RequestBody ProductDto productDto) {
         return productService.save(productDto);
+    }
+
+    @GetMapping("/{entityId}/image")
+    public ResponseEntity<byte[]> getImage(
+            @PathVariable("entityId") String id
+    ) {
+        Product product = productService.getProduct(id);
+        byte[] imageAsByteArray = product.getImageAsByteArray();
+        return ResponseEntity.status(HttpStatus.OK).contentLength(imageAsByteArray.length).body(imageAsByteArray);
+    }
+
+    @PostMapping("/{entityId}/image")
+    public ResponseEntity<Void> uploadImage(
+            @PathVariable("entityId") String id,
+            @RequestParam(value = "image") MultipartFile image
+    ) throws IOException {
+        byte[] imageAsByteArray = image.getBytes();
+        productService.saveImage(id, imageAsByteArray);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
+        return ResponseEntity.status(HttpStatus.CREATED).location(location).build();
     }
 
     @PutMapping
--- recipe-service/src/main/java/com/surmin/recipe/service/ProductService.java
+++ recipe-service/src/main/java/com/surmin/recipe/service/ProductService.java
@@ -1,16 +1,57 @@
 package com.surmin.recipe.service;
 
+import com.surmin.recipe.exception.EntityNotFoundException;
 import com.surmin.recipe.mapper.ProductMapper;
 import com.surmin.recipe.model.Product;
 import com.surmin.recipe.model.ProductDto;
 import com.surmin.recipe.repository.ProductRepository;
+import java.io.IOException;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService extends CrudService<ProductDto, Product, ProductRepository>{
 
+    private static final String PRODUCT_WAS_NOT_FOUND = "Product entity wasn't found";
+    private static final String IMAGE_IS_EMPTY = "Image can't be empty";
+    private static final String IMAGE_IS_OVERSIZED = "Image oversized, it should not exceed 1MB";
+
+    public static final int IMAGE_BYTES_LIMIT = 1 << 20;
+
     public ProductService(ProductRepository productRepository) {
         super(ProductMapper.INSTANCE, productRepository);
+    }
+
+    public Page<ProductDto> getPage(Integer pageId) {
+        Pageable pageable = PageRequest.of(pageId, 8);
+        return getAll(pageable);
+    }
+
+    public Product getProduct(String entityId) {
+        Optional<Product> productOptional = getMongoRepository().findById(entityId);
+        if (productOptional.isEmpty()) {
+            throw new EntityNotFoundException(PRODUCT_WAS_NOT_FOUND);
+        }
+        return productOptional.get();
+    }
+
+    public void saveImage(String entityId, byte[] imageAsByteArray) throws IOException {
+        if (imageAsByteArray == null || imageAsByteArray.length <= 0) {
+            throw new IllegalArgumentException(IMAGE_IS_EMPTY);
+        }
+        if (imageAsByteArray.length > IMAGE_BYTES_LIMIT) {
+            throw new IllegalArgumentException(IMAGE_IS_OVERSIZED);
+        }
+        Optional<Product> productOptional = getMongoRepository().findById(entityId);
+        if (productOptional.isEmpty()) {
+            throw new EntityNotFoundException(PRODUCT_WAS_NOT_FOUND);
+        }
+        Product product = productOptional.get();
+        product.setImageAsByteArray(imageAsByteArray);
+        getMongoRepository().save(product);
     }
 
 }
