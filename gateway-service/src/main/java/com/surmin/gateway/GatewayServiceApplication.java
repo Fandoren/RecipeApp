501609e4adfc9aafd097b0820181e8dc572cf1a8
Aleksei Surmin <aleks.fandoren@gmail.com>
2022-07-27 00:21:05 +0300

    remove zuul gateway. Add and implement cloud gateway. Temporarily move configuration from config repo

--- gateway-service/src/main/java/com/surmin/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/surmin/gateway/GatewayServiceApplication.java
@@ -2,10 +2,8 @@ package com.surmin.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
-@EnableZuulServer
 public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
