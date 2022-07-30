27ea2a2ee9ae584fffa1e54d32e0d661dc8acdba
Aleksei Surmin <aleks.fandoren@gmail.com>
2022-07-30 21:23:26 +0300

    add saveImage and getRecipe method in RecipeService. Implement corresponding endpoints in controller

--- recipe-service/src/main/java/com/surmin/recipe/controller/RecipeController.java
+++ recipe-service/src/main/java/com/surmin/recipe/controller/RecipeController.java
@@ -1,10 +1,14 @@
 package com.surmin.recipe.controller;
 
+import com.surmin.recipe.model.Recipe;
 import com.surmin.recipe.model.RecipeDto;
 import com.surmin.recipe.service.RecipeService;
+import java.io.IOException;
+import java.net.URI;
 import java.util.Collection;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +16,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/v1/recipes")
@@ -56,6 +63,26 @@ public class RecipeController {
     public void delete(@PathVariable("entityId") String entityId) {
         RecipeDto recipeDto = recipeService.get(entityId);
         recipeService.delete(recipeDto);
+    }
+
+    @GetMapping("/{entityId}/image")
+    public ResponseEntity<byte[]> getImage(
+            @PathVariable("entityId") String id
+    ) {
+        Recipe recipe = recipeService.getRecipe(id);
+        byte[] imageAsByteArray = recipe.getImageAsByteArray();
+        return ResponseEntity.status(HttpStatus.OK).contentLength(imageAsByteArray.length).body(imageAsByteArray);
+    }
+
+    @PostMapping("/{entityId}/image")
+    public ResponseEntity<Void> uploadImage(
+            @PathVariable("entityId") String id,
+            @RequestParam(value = "image") MultipartFile image
+    ) throws IOException {
+        byte[] imageAsByteArray = image.getBytes();
+        recipeService.saveImage(id, imageAsByteArray);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
+        return ResponseEntity.status(HttpStatus.CREATED).location(location).build();
     }
 
 }
--- recipe-service/src/main/java/com/surmin/recipe/service/RecipeService.java
+++ recipe-service/src/main/java/com/surmin/recipe/service/RecipeService.java
@@ -1,9 +1,12 @@
 package com.surmin.recipe.service;
 
+import com.surmin.recipe.exception.EntityNotFoundException;
 import com.surmin.recipe.mapper.RecipeMapper;
 import com.surmin.recipe.model.Recipe;
 import com.surmin.recipe.model.RecipeDto;
 import com.surmin.recipe.repository.RecipeRepository;
+import java.io.IOException;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -12,6 +15,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class RecipeService extends CrudService<RecipeDto, Recipe, RecipeRepository> {
 
+    private static final String RECIPE_WAS_NOT_FOUND = "Recipe entity wasn't found";
+    private static final String IMAGE_IS_EMPTY = "Image can't be empty";
+    private static final String IMAGE_IS_OVERSIZED = "Image oversized, it should not exceed 1MB";
+
+    public static final int IMAGE_BYTES_LIMIT = 1 << 20;
+
     public RecipeService(RecipeRepository recipeRepository) {
         super(RecipeMapper.INSTANCE, recipeRepository);
     }
@@ -19,5 +28,29 @@ public class RecipeService extends CrudService<RecipeDto, Recipe, RecipeReposito
     public Page<RecipeDto> getPage(Integer pageId) {
         Pageable pageable = PageRequest.of(pageId, 8);
         return getAll(pageable);
+    }
+
+    public Recipe getRecipe(String entityId) {
+        Optional<Recipe> recipeOptional = getMongoRepository().findById(entityId);
+        if (recipeOptional.isEmpty()) {
+            throw new EntityNotFoundException(RECIPE_WAS_NOT_FOUND);
+        }
+        return recipeOptional.get();
+    }
+
+    public void saveImage(String entityId, byte[] imageAsByteArray) throws IOException {
+        if (imageAsByteArray == null || imageAsByteArray.length <= 0) {
+            throw new IllegalArgumentException(IMAGE_IS_EMPTY);
+        }
+        if (imageAsByteArray.length > IMAGE_BYTES_LIMIT) {
+            throw new IllegalArgumentException(IMAGE_IS_OVERSIZED);
+        }
+        Optional<Recipe> recipeOptional = getMongoRepository().findById(entityId);
+        if (recipeOptional.isEmpty()) {
+            throw new EntityNotFoundException(RECIPE_WAS_NOT_FOUND);
+        }
+        Recipe recipe = recipeOptional.get();
+        recipe.setImageAsByteArray(imageAsByteArray);
+        getMongoRepository().save(recipe);
     }
 }
